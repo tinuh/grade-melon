c68b97a5d0dbcb19677e83e2039b96066b68db67
tinuh <tinuvanapamula@gmail.com>
2022-10-01 17:21:08 -0400

    do student vue api stuff

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
-import '../styles/globals.css'
+import React from "react"
+import '../styles/style.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./node_modules/flowbite-react/**/*.js",
+    "./pages/**/*.{ts,tsx}",
+    "./public/**/*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("flowbite/plugin"),
+  ],
+}
