9b7b214c7e88ef2946f1279ee0d1c1c0e16c1152
AlexL173 <alexliu13579@gmail.com>
2022-10-02 01:58:29 -0400

    Parse the Grades

--- pages/grades.tsx
+++ pages/grades.tsx
@@ -9,11 +9,45 @@ export default function Grades({ client }: GradesProps) {
 	const [grades, setGrades] = useState([]);
 	useEffect(() => {
 		client.gradebook().then((res) => {
-			console.log(res);
-			setGrades(res.courses);
+			//console.log(res);
+			setGrades(getInfoCurrent(res));
 			setLoading(false);
 		});
 	}, []);
 
 	return <div>{JSON.stringify(grades)}</div>;
 }
+
+function getInfoCurrent(data){
+    let array1 = Array(data.courses.length);
+    for (let i = 0; i < data.courses.length; i++){
+        let period = data.courses[i].period;
+        let courseName = data.courses[i].title;
+        let roomNumber = data.courses[i].room;
+        let teacherName = data.courses[i].staff.name;
+        let gradeNumber = data.courses[i].marks[0].calculatedScore.raw;
+
+        let letterGrade;
+        if (gradeNumber >= 89.5){
+            letterGrade = "A";
+        }
+        else if(gradeNumber >= 79.5){
+            letterGrade = "B";
+        }
+        else if (gradeNumber >= 69.5){
+            letterGrade = "C";
+        }
+        else if (gradeNumber >= 59.5){
+            letterGrade = "D";
+        }
+        else if (gradeNumber == 0){
+            letterGrade = "N/A"
+        }
+        else{
+            letterGrade = "E";
+        }
+        array1[i] = {period, courseName, roomNumber, teacherName, gradeNumber, letterGrade};
+    }
+    return array1;
+}
+
