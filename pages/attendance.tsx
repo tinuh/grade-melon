f5f34e1ac9548b2b947fa23007ab7252cd4d1eca
tinuh <tinuvanapamula@gmail.com>
2022-10-04 20:37:12 -0400

    basic chart attendance totals

---
+++ pages/attendance.tsx
@@ -0,0 +1,112 @@
+import React, { useState, useEffect } from "react";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { Spinner } from "flowbite-react";
+import { Bar } from "react-chartjs-2";
+import {
+	Chart as ChartJS,
+	CategoryScale,
+	LinearScale,
+	BarElement,
+	Title,
+	Tooltip,
+	Legend,
+} from "chart.js";
+
+ChartJS.register(
+	CategoryScale,
+	LinearScale,
+	BarElement,
+	Legend,
+	Title,
+	Tooltip
+);
+
+interface AttendanceProps {
+	client: any;
+}
+
+interface Attendance {
+	type: string;
+	periodInfos: [
+		{
+			period: number;
+			total: {
+				[key: string]: number;
+			};
+		}
+	];
+}
+
+export default function Attendance({ client }: AttendanceProps) {
+	const router = useRouter();
+	const [loading, setLoading] = useState(true);
+	const [data, setData] = useState<Attendance>();
+
+	useEffect(() => {
+		try {
+			client.attendance().then((res) => {
+				console.log(res);
+				setData(res);
+				setLoading(false);
+			});
+		} catch {
+			if (localStorage.getItem("remember") === "false") {
+				router.push("/login");
+			}
+		}
+	}, [client]);
+
+	const options = {
+		elements: {
+			bar: {
+				borderWidth: 1,
+			},
+		},
+		plugins: {
+			title: {
+				display: true,
+				text: "Attendance",
+			},
+		},
+		responsive: true,
+		scales: {
+			x: {
+				stacked: true,
+			},
+			y: {
+				stacked: true,
+			},
+		},
+	};
+
+	const colors = ["#50C878", "#50C878", "#FF7F7F", "#FF7F7F", "#FF7F7F"];
+	const chartData = {
+		labels: data?.periodInfos.map((periodInfo) => periodInfo.period),
+		datasets:
+			data &&
+			Object.keys(data.periodInfos[0].total).map((total, i) => {
+				return {
+					label: total.slice(0, 1).toUpperCase() + total.slice(1),
+					borderWidth: 1,
+					data: data.periodInfos.map((periodInfo) => periodInfo.total[total]),
+					backgroundColor: colors[i],
+				};
+			}),
+	};
+
+	return (
+		<div className="p-10 h-full">
+			<Head>
+				<title>Attendance - Grade Melon</title>
+			</Head>
+			{loading ? (
+				<Spinner size="xl" />
+			) : (
+				<div className="w-96">
+					<Bar options={options} data={chartData} />
+				</div>
+			)}
+		</div>
+	);
+}
--- pages/documents.tsx
+++ pages/documents.tsx
@@ -2,122 +2,121 @@ import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import Head from "next/head";
 import { Spinner } from "flowbite-react";
-import { triggerBase64Download } from "common-base64-downloader-react";
+
+const base64toBlob = (base64Data: string) => {
+	const sliceSize = 1024;
+	const byteCharacters = atob(base64Data);
+	const bytesLength = byteCharacters.length;
+	const slicesCount = Math.ceil(bytesLength / sliceSize);
+	const byteArrays = new Array(slicesCount);
+
+	for (let sliceIndex = 0; sliceIndex < slicesCount; ++sliceIndex) {
+		const begin = sliceIndex * sliceSize;
+		const end = Math.min(begin + sliceSize, bytesLength);
+
+		const bytes = new Array(end - begin);
+		for (let offset = begin, i = 0; offset < end; ++i, ++offset) {
+			bytes[i] = byteCharacters[offset].charCodeAt(0);
+		}
+		byteArrays[sliceIndex] = new Uint8Array(bytes);
+	}
+	return new Blob(byteArrays, { type: "application/pdf" });
+};
+
+const openBase64NewTab = (base64Pdf: string): void => {
+	let blob = base64toBlob(base64Pdf);
+	if (typeof window !== "undefined") {
+		if (window.navigator && (window.navigator as any).msSaveOrOpenBlob) {
+			(window.navigator as any).msSaveOrOpenBlob(blob, "pdfBase64.pdf");
+		} else {
+			const blobUrl = URL.createObjectURL(blob);
+			window.open(blobUrl);
+		}
+	}
+};
 
 interface DocumentsProps {
-  client: any;
+	client: any;
 }
 
 export default function Documents({ client }: DocumentsProps) {
-  const router = useRouter();
-  const [documents, setDocuments] = useState([]);
-  const [loading, setLoading] = useState(true);
+	const router = useRouter();
+	const [documents, setDocuments] = useState([]);
+	const [loading, setLoading] = useState(true);
 
-  const openBase64NewTab = (base64Pdf: string): void => {
-    let blob = base64toBlob(base64Pdf);
-    if (typeof window !== "undefined") {
-      if (window.navigator && (window.navigator as any).msSaveOrOpenBlob) {
-        (window.navigator as any).msSaveOrOpenBlob(blob, "pdfBase64.pdf");
-      } else {
-        const blobUrl = URL.createObjectURL(blob);
-        window.open(blobUrl);
-      }
-    }
-  }
-  
-  const base64toBlob = (base64Data: string) => {
-    const sliceSize = 1024;
-    const byteCharacters = atob(base64Data);
-    const bytesLength = byteCharacters.length;
-    const slicesCount = Math.ceil(bytesLength / sliceSize);
-    const byteArrays = new Array(slicesCount);
-  
-    for (let sliceIndex = 0; sliceIndex < slicesCount; ++sliceIndex) {
-      const begin = sliceIndex * sliceSize;
-      const end = Math.min(begin + sliceSize, bytesLength);
-  
-      const bytes = new Array(end - begin);
-      for (let offset = begin, i = 0; offset < end; ++i, ++offset) {
-        bytes[i] = byteCharacters[offset].charCodeAt(0);
-      }
-      byteArrays[sliceIndex] = new Uint8Array(bytes);
-    }
-    return new Blob(byteArrays, { type: "application/pdf" });
-  }
+	useEffect(() => {
+		try {
+			client.documents().then((res) => {
+				console.log(res);
+				setDocuments(res);
+				setLoading(false);
+			});
+		} catch {
+			if (localStorage.getItem("remember") === "false") {
+				router.push("/login");
+			}
+		}
+	}, [client]);
 
-  useEffect(() => {
-    try {
-      client.documents().then((res) => {
-        console.log(res);
-        setDocuments(res);
-        setLoading(false);
-      });
-    } catch {
-      if (localStorage.getItem("remember") === "false") {
-        router.push("/login");
-      }
-    }
-  }, [client]);
-
-  return (
-    <div className="p-10 h-full">
-      <Head>
-        <title>Schedule - Grade Melon</title>
-      </Head>
-      {loading ? (
-        <Spinner size="xl" />
-      ) : (
-        <div className="overflow-x-auto shadow-md sm:rounded-lg">
-          <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-              <tr>
-                <th scope="col" className="py-3 pl-6">
-                  Date
-                </th>
-                <th scope="col" className="py-3 px-6">
-                  Document
-                </th>
-                <th scope="col" className="py-3 px-6">
-                  Category
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {documents &&
-                documents.map((document, i) => (
-                  <tr
-                    className={`bg-${
-                      i % 2 == 0 ? "white" : "gray-50"
-                    } border-b dark:bg-gray-${
-                      i % 2 == 0 ? 900 : 800
-                    } dark:border-gray-700`}
-                    key={i}
-                  >
-                    <th
-                      scope="row"
-                      className="py-4 pl-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                    >
-                      {document.file.date.toLocaleDateString()}
-                    </th>
-                    <td className="py-4 px-6">
-                      <a
-                        onClick={async () => {
-                          let download = await document.get();
-                          console.log(download)
-                          openBase64NewTab(download[0].base64);
-                        }}
-                        href="#"
-                      >
-                        {document.comment}
-                      </a>
-                    </td>
-                    <td className="py-4 px-6">{document.file.type}</td>
-                  </tr>
-                ))}
-            </tbody>
-          </table>
-        </div>
-      )}
-    </div>
-  );
+	return (
+		<div className="p-10 h-full">
+			<Head>
+				<title>Documents - Grade Melon</title>
+			</Head>
+			{loading ? (
+				<Spinner size="xl" />
+			) : (
+				<div className="overflow-x-auto shadow-md sm:rounded-lg">
+					<table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+						<thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+							<tr>
+								<th scope="col" className="py-3 pl-6">
+									Date
+								</th>
+								<th scope="col" className="py-3 px-6">
+									Document
+								</th>
+								<th scope="col" className="py-3 px-6">
+									Category
+								</th>
+							</tr>
+						</thead>
+						<tbody>
+							{documents &&
+								documents.map((document, i) => (
+									<tr
+										className={`bg-${
+											i % 2 == 0 ? "white" : "gray-50"
+										} border-b dark:bg-gray-${
+											i % 2 == 0 ? 900 : 800
+										} dark:border-gray-700`}
+										key={i}
+									>
+										<th
+											scope="row"
+											className="py-4 pl-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+										>
+											{document.file.date.toLocaleDateString()}
+										</th>
+										<td className="py-4 px-6">
+											<a
+												onClick={async () => {
+													let download = await document.get();
+													console.log(download);
+													openBase64NewTab(download[0].base64);
+												}}
+												href="#"
+											>
+												{document.comment}
+											</a>
+										</td>
+										<td className="py-4 px-6">{document.file.type}</td>
+									</tr>
+								))}
+						</tbody>
+					</table>
+				</div>
+			)}
+		</div>
+	);
 }
