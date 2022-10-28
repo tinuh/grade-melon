04577c39f21191cd99d9d3f20303552575dc691f
tinuvanapamula@gmail.com <tinuvanapamula@gmail.com>
2022-10-28 14:26:59 -0400

    fix padding on mobile

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -35,7 +35,7 @@ export default function TopBar({ studentInfo, logout }: TopBarProps) {
 								label={
 									<button
 										type="button"
-										className="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+										className="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
 										id="user-menu-button"
 										aria-expanded="false"
 										data-dropdown-toggle="user-dropdown"
