0b7fee048fd514195e5654848f2470e05a959638
tinuvanapamula@gmail.com <tinuvanapamula@gmail.com>
2023-01-29 22:38:14 -0500

    fix build issue

--- pages/privacy.tsx
+++ pages/privacy.tsx
@@ -141,7 +141,7 @@ export default function privacy() {
 			<h3 className="py-3 font-semibold text-lg">Usage Data</h3>
 			<p>Usage Data is collected automatically when using the Service.</p>
 			<p>
-				Usage Data may include information such as Your Device's Internet
+				Usage Data may include information such as Your Device&apos;s Internet
 				Protocol address (e.g. IP address), browser type, browser version, the
 				pages of our Service that You visit, the time and date of Your visit,
 				the time spent on those pages, unique device identifiers and other
@@ -279,7 +279,7 @@ export default function privacy() {
 					<p>
 						<strong>To contact You:</strong> To contact You by email, telephone
 						calls, SMS, or other equivalent forms of electronic communication,
-						such as a mobile application's push notifications regarding updates
+						such as a mobile application&apos;s push notifications regarding updates
 						or informative communications related to the functionalities,
 						products or contracted services, including the security updates,
 						when necessary or reasonable for their implementation.
@@ -375,7 +375,7 @@ export default function privacy() {
 			</p>
 			<h2 className="py-3 font-semibold text-xl">Transfer of Your Personal Data</h2>
 			<p>
-				Your information, including Personal Data, is processed at the Company's
+				Your information, including Personal Data, is processed at the Company&apos;s
 				operating offices and in any other places where the parties involved in
 				the processing are located. It means that this information may be
 				transferred to — and maintained on — computers located outside of Your
@@ -452,7 +452,7 @@ export default function privacy() {
 				means to protect Your Personal Data, We cannot guarantee its absolute
 				security.
 			</p>
-			<h1 className="pt-5 font-bold text-3xl">Children's Privacy</h1>
+			<h1 className="pt-5 font-bold text-3xl">Children&apos;s Privacy</h1>
 			<p>
 				Our Service does not address anyone under the age of 13. We do not
 				knowingly collect personally identifiable information from anyone under
@@ -465,14 +465,14 @@ export default function privacy() {
 			<p>
 				If We need to rely on consent as a legal basis for processing Your
 				information and Your country requires consent from a parent, We may
-				require Your parent's consent before We collect and use that
+				require Your parent&apos;s consent before We collect and use that
 				information.
 			</p>
 			<h1 className="pt-5 font-bold text-3xl">Links to Other Websites</h1>
 			<p>
 				Our Service may contain links to other websites that are not operated by
 				Us. If You click on a third party link, You will be directed to that
-				third party's site. We strongly advise You to review the Privacy Policy
+				third party&apos;s site. We strongly advise You to review the Privacy Policy
 				of every site You visit.
 			</p>
 			<p>
